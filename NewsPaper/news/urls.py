073fd5c475bf179eb6261afa95633079ce9f7a84
Alexande Syachin <asyachin@gmail.com>
2023-10-22 22:19:42 +0300

    Modul D3 HW-03

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -3,6 +3,5 @@ from .views import NewsList, NewsDetail
 
 urlpatterns = [
     path('', NewsList.as_view()),  # Matches the root URL ('/news/') and uses the NewsList view
-    #path('<int:pk>', NewsDetail.as_view()),  # Matches URLs with an integer primary key, using the NewsDetail view
     path('<int:pk>/', NewsDetail.as_view(), name='news_detail'),
 ]
