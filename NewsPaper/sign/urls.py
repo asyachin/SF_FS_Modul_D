8c63cd8095a9439640c89189ae580c7f327061e4
Alexande Syachin <asyachin@gmail.com>
2023-11-22 10:10:16 +0200

    add protect + sign app

--- NewsPaper/sign/urls.py
+++ NewsPaper/sign/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from .views import RegisterView, LoginView, LogoutView
 
 app_name = 'sign'
 urlpatterns = [
-   path('login/',
-       LoginView.as_view(template_name='sign/login.html'),
-       name='login'),
-   path('logout/',
-       LogoutView.as_view(template_name='sign/logout.html'),
-       name='logout'),
+   path('login/', LoginView.as_view(), name='login'),
+   path('logout/', LogoutView.as_view(), name='logout'),
+   path('signup/', RegisterView.as_view(), name='signup')
 ]
