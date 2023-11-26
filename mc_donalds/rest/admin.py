392f53cb98f9b3e798cfda33a4907f6e1937ebb8
Alexande Syachin <asyachin@gmail.com>
2023-11-26 21:00:45 +0200

    D5.7 HW-03

--- mc_donalds/rest/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Order, Product, Staff, ProductOrder
-
-admin.site.register(Order)
-admin.site.register(Product)
-admin.site.register(Staff)
-admin.site.register(ProductOrder)
--- mc_donalds/rest/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ProductsList, ProductDetail # импортируем наше представление
- 
- 
- 
-urlpatterns = [
-    path('', ProductsList.as_view()),
-    path('<int:pk>', ProductDetail.as_view()),  # pk -- это первичный ключ товара, который будет выводиться у нас в шаблон
-]
