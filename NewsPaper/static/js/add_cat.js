a523291983272a4193f96aa18ef23df6af5f8263
Alexande Syachin <asyachin@gmail.com>
2023-12-07 10:01:27 +0200

    07.12.2023 added Subscription

---
+++ NewsPaper/static/js/add_cat.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById('addCategoryBtn').addEventListener('click', function() {
+			document.getElementById('categoryModal').style.display = 'block';
+	});
+});
